498a8cbf412e188bfd99da2c6a1aea7a02869505
NicoEtche2004 <netchepare@udesa.edu.ar>
2024-04-15 15:46:58 -0300

    smallupdate

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var maincontroller = require('../controllers/firstController')
+var maincontroller = require('../controllers/usersController')
 
 /* GET home page. */
 router.get('/', maincontroller.main);
