28502965fa7e502eb2cfb2e02decf1bf7c341588
ManuelSaccullo <msaccullo@udesa.edu.ar>
2024-04-15 16:35:25 -0300

    Database de productos

---
+++ db/products.js
@@ -0,0 +1,62 @@
+const productos = [
+    {
+      id: 1,
+      imagen: "imagen1.jpg",
+      nombre: "Camiseta de algodón",
+      descripcion: "Camiseta de algodón suave y cómoda, ideal para uso diario.",
+    },
+    {
+      id: 2,
+      imagen: "imagen2.jpg",
+      nombre: "Pantalones vaqueros ajustados",
+      descripcion: "Pantalones vaqueros de corte ajustado para un look moderno y casual.",
+    },
+    {
+      id: 3,
+      imagen: "imagen3.jpg",
+      nombre: "Zapatos deportivos",
+      descripcion: "Zapatos deportivos ligeros y transpirables, perfectos para hacer ejercicio.",
+    },
+    {
+      id: 4,
+      imagen: "imagen4.jpg",
+      nombre: "Bolso de cuero",
+      descripcion: "Bolso de cuero elegante y espacioso, ideal para llevar tus pertenencias con estilo.",
+    },
+    {
+      id: 5,
+      imagen: "imagen5.jpg",
+      nombre: "Reloj de pulsera",
+      descripcion: "Reloj de pulsera elegante con correa de acero inoxidable y mecanismo de cuarzo.",
+    },
+    {
+      id: 6,
+      imagen: "imagen6.jpg",
+      nombre: "Gafas de sol",
+      descripcion: "Gafas de sol con montura de acetato y lentes polarizadas para una protección óptima.",
+    },
+    {
+      id: 7,
+      imagen: "imagen7.jpg",
+      nombre: "Cartera de cuero",
+      descripcion: "Cartera de cuero compacta con múltiples compartimentos para organizar tus tarjetas y dinero.",
+    },
+    {
+      id: 8,
+      imagen: "imagen8.jpg",
+      nombre: "Chaqueta de cuero",
+      descripcion: "Chaqueta de cuero resistente y duradera, perfecta para lucir un estilo urbano.",
+    },
+    {
+      id: 9,
+      imagen: "imagen9.jpg",
+      nombre: "Pendientes de plata",
+      descripcion: "Pendientes de plata fina con incrustaciones de circonitas, ideales para ocasiones especiales.",
+    },
+    {
+      id: 10,
+      imagen: "imagen10.jpg",
+      nombre: "Mochila urbana",
+      descripcion: "Mochila urbana con diseño moderno y compartimentos acolchados para proteger tus dispositivos.",
+    }
+  ];
