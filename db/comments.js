cbe0be1cbe7cccdde185269032594295bc5ea674
joacobaiardi <jbaiardi@udesa.edu.ar>
2024-04-15 17:11:20 -0300

    Database comments

---
+++ db/comments.js
@@ -0,0 +1,40 @@
+const productos = [
+    {
+      id: 1,
+      nombre: "Producto 1",
+      precio: 100,
+      descripcion: "Descripción del producto 1.",
+      imagen: "ruta/a/imagen1.jpg",
+      comentarios: [
+        {
+          nombreUsuario: "Usuario1",
+          textoComentario: "Este producto es genial!",
+          imagenPerfil: "ruta/a/imagenPerfil1.jpg"
+        },
+        {
+          nombreUsuario: "Usuario2",
+          textoComentario: "Me encanta este producto!",
+          imagenPerfil: "ruta/a/imagenPerfil2.jpg"
+        }
+      ]
+    },
+    {
+      id: 2,
+      nombre: "Producto 2",
+      precio: 150,
+      descripcion: "Descripción del producto 2.",
+      imagen: "ruta/a/imagen2.jpg",
+      comentarios: [
+        {
+          nombreUsuario: "Usuario3",
+          textoComentario: "Buena calidad.",
+          imagenPerfil: "ruta/a/imagenPerfil3.jpg"
+        },
+        {
+          nombreUsuario: "Usuario4",
+          textoComentario: "Lo recomiendo totalmente.",
+          imagenPerfil: "ruta/a/imagenPerfil4.jpg"
+        }
+      ]
+    }
+  ];
