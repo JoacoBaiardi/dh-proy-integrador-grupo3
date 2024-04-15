14c541ca90206e6b087afe6f7810bb70b00846f1
joacobaiardi <jbaiardi@udesa.edu.ar>
2024-04-15 16:12:16 -0300

    Hice database de usuarios

---
+++ db/usuarios.js
@@ -0,0 +1,94 @@
+const usuarios = [
+    {
+      id: 1,
+      email: "usuario1@example.com",
+      usuario: "usuario1",
+      password: "contraseña1",
+      fechaNacimiento: "1990-01-01",
+      numeroDocumento: "12345678A",
+      fotoPerfil: "ruta/a/foto1.jpg"
+    },
+    {
+      id: 2,
+      email: "usuario2@example.com",
+      usuario: "usuario2",
+      password: "contraseña2",
+      fechaNacimiento: "1991-02-02",
+      numeroDocumento: "23456789B",
+      fotoPerfil: "ruta/a/foto2.jpg"
+    },
+    {
+      id: 3,
+      email: "usuario3@example.com",
+      usuario: "usuario3",
+      password: "contraseña3",
+      fechaNacimiento: "1992-03-03",
+      numeroDocumento: "34567890C",
+      fotoPerfil: "ruta/a/foto3.jpg"
+    },
+    {
+      id: 4,
+      email: "usuario4@example.com",
+      usuario: "usuario4",
+      password: "contraseña4",
+      fechaNacimiento: "1993-04-04",
+      numeroDocumento: "45678901D",
+      fotoPerfil: "ruta/a/foto4.jpg"
+    },
+    {
+      id: 5,
+      email: "usuario5@example.com",
+      usuario: "usuario5",
+      password: "contraseña5",
+      fechaNacimiento: "1994-05-05",
+      numeroDocumento: "56789012E",
+      fotoPerfil: "ruta/a/foto5.jpg"
+    },
+    {
+      id: 6,
+      email: "usuario6@example.com",
+      usuario: "usuario6",
+      password: "contraseña6",
+      fechaNacimiento: "1995-06-06",
+      numeroDocumento: "67890123F",
+      fotoPerfil: "ruta/a/foto6.jpg"
+    },
+    {
+      id: 7,
+      email: "usuario7@example.com",
+      usuario: "usuario7",
+      password: "contraseña7",
+      fechaNacimiento: "1996-07-07",
+      numeroDocumento: "78901234G",
+      fotoPerfil: "ruta/a/foto7.jpg"
+    },
+    {
+      id: 8,
+      email: "usuario8@example.com",
+      usuario: "usuario8",
+      password: "contraseña8",
+      fechaNacimiento: "1997-08-08",
+      numeroDocumento: "89012345H",
+      fotoPerfil: "ruta/a/foto8.jpg"
+    },
+    {
+      id: 9,
+      email: "usuario9@example.com",
+      usuario: "usuario9",
+      password: "contraseña9",
+      fechaNacimiento: "1998-09-09",
+      numeroDocumento: "90123456I",
+      fotoPerfil: "ruta/a/foto9.jpg"
+    },
+    {
+      id: 10,
+      email: "usuario10@example.com",
+      usuario: "usuario10",
+      password: "contraseña10",
+      fechaNacimiento: "1999-10-10",
+      numeroDocumento: "01234567J",
+      fotoPerfil: "ruta/a/foto10.jpg"
+    }
+  ];
+
+  
